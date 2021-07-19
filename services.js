4646a8e101d051926bddcd77accc65d82fb1ddf2
Jose Soto <jsoto7087@gmail.com>
2021-07-19 09:46:44 -0400

    refractor fetch in to a function

--- services.js
+++ services.js
@@ -1,18 +1,19 @@
 const URL = 'http://localhost:3000/payroll'
 
+async function fetEmployeeInfo() {
+    const response = await fetch(URL, {
+        headers: {
+            'Accepts': 'application/json'
+        }
+    })
+    .then(res => res.json())
 
-fetch('http://localhost:3000/payroll')
-.then(response => response.json())
-.then(data => {
-
-console.log(data);
-
-const ul = document.querySelector('#time_sheet')
-data.forEach(payroll => {
-    const timeSheet = `${payroll.id} ${payroll.rate} ${payroll.overTime}`;
-    const li = document.createElement('li')
-    li.textContent = timeSheet
-    ul.append(li);
-})
-
-})
+    const data = await response.json();
+    console.log(data);
+    const ul = document.querySelector('#time_sheet');
+    data.forEach(payroll => {
+        const timeSheet = `${payroll.id} ${payroll.rate} ${payroll.overTime}`;
+        const li = document.createElement('li');
+        li.textContent = timeSheet;
+        ul.append(li);
+    });
