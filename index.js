0cc00b9982cf5006e295ff46d20ad5e366ba23a4
Jose Soto <jsoto7087@gmail.com>
2021-07-19 16:02:00 -0400

    fixded buttons

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+// const URL = 'http://localhost:3000/payroll';
+
+// fetch(URL)
+// .then(response => response.json())
+// .then(data => {
+
+//  console.log(data);
+//   const ul = document.querySelector('#load_info_1');
+
+// const userInfo = document.querySelector('#load_info_1')
+// userInfo.parentElement.addEventListener('click', (event)=> {
+//   event.preventDefault()
+
+//   data.find(employeeI => {
+//     const empInfo = `${employeeI.info1}`;
+//     const li = document.createElement('li')
+//     li.textContent = empInfo
+//     ul.append(li);
+//   })
+// })
+// })
+
+
+
+
+
+
+
 
 // document.getElementById("lastN").addEventListener("submit", userInput);
 
--- services.js
+++ services.js
@@ -3,17 +3,23 @@ const URL = 'http://localhost:3000/payroll';
 fetch(URL)
 .then(response => response.json())
 .then(data => {
+
   console.log(data);
   const ul = document.querySelector('#time_sheet');
 
-const userInfo = document.querySelector('#load_info_1')
-userInfo.parentElement.addEventListener('click', (event)=> {
-  event.preventDefault()
-  data.filter(employeeI => {
-    const empInfo = `${employeeI.info1}`;
-    const li = document.createElement('li')
-    li.textContent = empInfo
-    ul.append(li);
-  })
+    const userInfo = document.querySelector('#load_info_1')
+    userInfo.parentElement.addEventListener('click', (event)=> {
+        event.preventDefault()
+
+        const foundEmp = data.find(employee => {
+            return userInfo.innerText === employee.fullName
+        })
+        
+        const empInfo = `${foundEmp.id} ${foundEmp.fullName}`;
+        const li = document.createElement('li')
+        li.textContent = empInfo
+        ul.append(li);
+    })
 })
-})
+
+
