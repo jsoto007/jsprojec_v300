0ebbe9326241ef3cf684396285d636f93ce31204
Jose Soto <jsoto7087@gmail.com>
2021-07-19 10:02:46 -0400

    added post

--- services.js
+++ services.js
@@ -3,17 +3,32 @@ const URL = 'http://localhost:3000/payroll'
 async function fetEmployeeInfo() {
     const response = await fetch(URL, {
         headers: {
-            'Accepts': 'application/json'
+            'Accept': 'application/json'
         }
     })
     .then(res => res.json())
+}
 
-    const data = await response.json();
-    console.log(data);
-    const ul = document.querySelector('#time_sheet');
-    data.forEach(payroll => {
-        const timeSheet = `${payroll.id} ${payroll.rate} ${payroll.overTime}`;
-        const li = document.createElement('li');
-        li.textContent = timeSheet;
-        ul.append(li);
-    });
+function postEmployeeInfo() {
+    return fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json'
+    },
+    body: JSON.stringify(body)
+})
+
+    .then(res => res.json())
+}
+
+
+
+    // const data = await response.json();
+    // console.log(data);
+    // const ul = document.querySelector('#time_sheet');
+    // data.forEach(payroll => {
+    //     const timeSheet = `${payroll.id} ${payroll.rate} ${payroll.overTime}`;
+    //     const li = document.createElement('li');
+    //     li.textContent = timeSheet;
+    //     ul.append(li);
+    // });
